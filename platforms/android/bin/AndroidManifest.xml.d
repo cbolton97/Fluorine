2389d8bf2c18d8c90e385593dafe181f2a723ff7
Chris Bolton <chris.bolton97@gmail.com>
2013-11-28 19:47:21 -0800

    Initial commit of phone gap files.

---
+++ platforms/android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/user/Dev/Fluorine/platforms/android/bin/AndroidManifest.xml : \
+/Users/user/Dev/Fluorine/platforms/android/AndroidManifest.xml \
---
+++ platforms/android/bin/classes.dex.d
@@ -0,0 +1,10 @@
+/Users/user/Dev/Fluorine/platforms/android/bin/classes.dex : \
+/Users/user/Dev/Fluorine/platforms/android/bin/classes/com/codeandconfusion/fluorine/BuildConfig.class \
+/Users/user/Dev/Fluorine/platforms/android/bin/classes/com/codeandconfusion/fluorine/Fluorine.class \
+/Users/user/Dev/Fluorine/platforms/android/bin/classes/com/codeandconfusion/fluorine/R$attr.class \
+/Users/user/Dev/Fluorine/platforms/android/bin/classes/com/codeandconfusion/fluorine/R$drawable.class \
+/Users/user/Dev/Fluorine/platforms/android/bin/classes/com/codeandconfusion/fluorine/R$layout.class \
+/Users/user/Dev/Fluorine/platforms/android/bin/classes/com/codeandconfusion/fluorine/R$string.class \
+/Users/user/Dev/Fluorine/platforms/android/bin/classes/com/codeandconfusion/fluorine/R$xml.class \
+/Users/user/Dev/Fluorine/platforms/android/bin/classes/com/codeandconfusion/fluorine/R.class \
+/Users/user/Dev/Fluorine/platforms/android/bin/dexedLibs/cordova-3.0.0-d206ac4eeaa68e8d568b785a2acc2ee9.jar \
